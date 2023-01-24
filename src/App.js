748eefc10352ab087ac0d887c545c7ddee17da11
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2023-01-24 17:47:12 -0300

    button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
